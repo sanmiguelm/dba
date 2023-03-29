2e87504c77518565ea23585b777e1ed26b5afeb7
sanmiguelm <129054441+sanmiguelm@users.noreply.github.com>
2023-03-29 08:49:48 +0800

    Add files via upload

---
+++ Exam2.java
@@ -0,0 +1,15 @@
+package dba_exam;
+
+public class Exam2 {
+
+	public static void main(String []args) {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print(i + 1);
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+}
